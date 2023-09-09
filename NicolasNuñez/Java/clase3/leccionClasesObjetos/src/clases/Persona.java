27fcb1d7aca6f84d1ce801956cd2d57dd43f3653
Nicolas Nuñez <nuneznicolas582@gmail.com>
2023-09-09 20:02:49 -0300

    Se modifica el archivo de la clase 3 con lo visto en la clase 4 sobre objetos en Java

--- NicolasNuñez/Java/clase3/leccionClasesObjetos/src/clases/Persona.java
+++ NicolasNuñez/Java/clase3/leccionClasesObjetos/src/clases/Persona.java
@@ -6,4 +6,11 @@ public class Persona {
     //atributos
     String nombre;
     String apellido;
+    
+    //metodos
+    
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
