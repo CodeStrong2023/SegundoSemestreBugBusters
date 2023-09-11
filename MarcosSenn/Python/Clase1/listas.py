5a55160e4d7f08a0c6434f0d6161a416f4aac316
marcos-senn <marcossenn@gmail.com>
2023-09-10 21:41:32 -0300

    python y JS completo

---
+++ MarcosSenn/Python/Clase1/listas.py
@@ -0,0 +1,22 @@
+#Funciones de listas
+#append
+#len
+#insert(index,elemento)
+#remove(´elemento´)
+#pop()
+#clear()
+
+#Ejercicio 1
+print('Ejercicio1')
+for numero in range(3,11,2):
+    print(numero)
+
+#Ejercicio 2
+print('Ejercicio 2')
+for numero in range(0,11,3):
+    print(numero)
+
+#Ejercicio 3
+print('Ejercicio 3')
+for numero in range(2,7):
+    print(numero)
---
+++ MarcosSenn/Python/Clase3/ejericicos.py
@@ -0,0 +1,7 @@
+seleccionArgentina = {
+    1:{"Nombre": "Franco", "Edad":20, "Altura":1.65, "Precio":'10 pesos'},
+    2:{"Nombre": "Lucas", "Edad":26, "Altura":1.95, "Precio":'60 pesos'},
+    3: {"Nombre": "Brito", "Edad":33, "Altura":1.70, "Precio":'90 pesos'},
+    4: {"Nombre": "Ameal", "Edad":150, "Altura":1.85, "Precio":'12 pesos'},
+}
+
