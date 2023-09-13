e00f64666caaa4b1f969cdbfed1f9883cadba2c6
Nicolas Nuñez <nuneznicolas582@gmail.com>
2023-09-13 20:21:32 -0300

    Se modifica el archivo de la clase 4 con lo nuevo de metodos visto en la clase 5 de Java

--- NicolasNuñez/Java/clase4/src/Operaciones/Aritmetica.java
+++ NicolasNuñez/Java/clase4/src/Operaciones/Aritmetica.java
@@ -12,4 +12,18 @@ public class Aritmetica {
         int suma = a + b;
         System.out.println("suma = " + suma);
     }
+    
+    public int sumarConRetorno(){
+        int resultado = a + b; 
+        return resultado; // return a + b;
+    }
+    
+    public int sumarConArgumentos(int num1, int num2){
+        a = num1; //no modifica los valores de los atributos del objeto
+        b = num2;
+        //return a + b;
+        return sumarConRetorno(); //llamamos un metodo dentro de otro metodo
+    }
+    
+    
 }
