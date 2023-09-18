bbdc52a8dcb23643f23bbc94cfb0d635ea2deafa
Nicolas Nuñez <nuneznicolas582@gmail.com>
2023-09-18 20:29:03 -0300

    Subo la clase 5 de JavaScript

--- NicolasNuñez/JavaScript/clase5/clase5.js
+++ NicolasNuñez/JavaScript/clase5/clase5.js
@@ -1,12 +1,24 @@
 
 //Tipo de dato Undefined
 
-let x; //se asigna undefined al no darle un valor a la variable
+let x;
+x = undefined; //se asigna undefined al no darle un valor a la variable
 console.log(x)
 console.log(typeof x)
 
 //null --> es la ausencia de un valor
 
-let y = null; //null no es un tipo de dato, pero su origen es object
+let y = null; //null no es un tipo de dato, pero su origen es de tipo object
 console.log(y)
 console.log(typeof y)
+
+//Tipo de dato Array --> son de tipo object
+
+let autos = ["Peugeot", "Fiat", "Chevrolet", "BMW"]
+console.log(autos)
+console.log(typeof autos)
+
+//Empty String
+let z = ""; //Es una cadena vacia
+console.log(z)
+console.log(typeof z)
