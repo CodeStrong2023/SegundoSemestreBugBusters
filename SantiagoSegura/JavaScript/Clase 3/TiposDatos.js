a49735a24e55d2387bb0f80a97865de6adc298d8
Santiago Segura <santiagosegura15@hotmail.com>
2023-10-02 22:39:25 -0300

    Segundas clases de Python, Java y JavaScript

---
+++ SantiagoSegura/JavaScript/Clase 3/TiposDatos.js
@@ -0,0 +1,18 @@
+// Tipos de Datos en JavaScript
+/*
+La sintais en lo que es comentarios
+es muy similar a la de Java
+realmente diriamos que es identica
+*/
+var nombre = "Ariel"; //Tipo Str
+console.log(nombre);
+
+var numero = 3000; //Tipo Numérico
+console.log(numero);
+
+var objeto = {
+    nombre : "Santiago",
+    apellido : "Segura",
+    telefono : "2604567890",
+}
+console.log(objeto);
