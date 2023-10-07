4e95d61b4bc1ae523f967dc784a73fa655eb6505
marcos-senn <marcossenn@gmail.com>
2023-10-07 19:01:46 -0300

    Clase 7 JS

---
+++ MarcosSenn/JavaScript/Clase7/encontrarParImpar.js
@@ -0,0 +1,8 @@
+const parImpar = (num) => {
+    if(num % 2 === 0){
+        console.log('par') }
+    else console.log('impar')
+}
+
+
+parImpar(23) // cambiar el numero
---
+++ MarcosSenn/JavaScript/Clase7/esMayorDeEdad.js
@@ -0,0 +1,8 @@
+const esMayorOmenor = (edad) => {
+    if(edad>=18){
+        console.log("Es mayor de edad")}
+    else console.log("Es menor de edad")
+}
+
+
+esMayorOmenor(60) // cambiar el numero
