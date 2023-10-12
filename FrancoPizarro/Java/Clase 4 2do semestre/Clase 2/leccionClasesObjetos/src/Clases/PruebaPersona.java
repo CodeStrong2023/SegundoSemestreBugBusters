3aad200ba1c2bdf32fa8116dec2dc96bdb634f4c
Franco <pizarrofranco25@gmail.com>
2023-10-12 15:52:52 -0300

    Clase 4 2do semestre en Java (Métodos, creacion de objetos, Ejercicios 6 y 7, nueva LECCION 4)

---
+++ FrancoPizarro/Java/Clase 4 2do semestre/Clase 2/leccionClasesObjetos/src/Clases/PruebaPersona.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Franco
+ */
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona(); //Llamamos al constructor
+        persona1.nombre = "Franco";
+        persona1.apellido = "Pizarro";
+        persona1.obtenerInformacion();
+        
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2);
+        System.out.println("persona1 = " + persona1);
+        persona2.obtenerInformacion();
+        persona2.nombre = "Juan Cruz";
+        persona2.apellido = "Cogo";
+        persona2.obtenerInformacion();
+    }
+}
