76a3f3d9dd68b42997274ca0c68c706d1a9ec673
Franco <pizarrofranco25@gmail.com>
2023-10-26 18:23:04 -0300

    Clase 3 2do semestre en JavaScript (Tipos de datos str, númerico y objeto, funcion alert)

---
+++ FrancoPizarro/Javascript/Leccion2/02-01-TiposDeDatos.js
@@ -0,0 +1,17 @@
+// Tipos de datos en JavaScript
+/* 
+La sintaxis en lo que es comentarios es muy similar  a la
+de Java, realmente diriamos que es identica
+*/
+var nombre = "Franco"; //Tipo String
+console.log(nombre);
+
+var edad = 19;
+console.log(edad);
+
+var objeto = {
+    nombre : "Franco",
+    apellido : "Pizarro",
+    telefono : "2604817007"
+}
+console.log(objeto);
