1042f9daf577b79c20c8a94f3c4d0d442e872d67
Santiago Segura <santiagosegura15@hotmail.com>
2023-11-02 13:46:16 -0300

    Agrego la clase 9 de js

--- SantiagoSegura/JavaScript/Clase7/03-01-OperadoresJavaScript-UJS.js
+++ SantiagoSegura/JavaScript/Clase7/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,37 @@ if(dentroRango >= valMin %% dentroRango <= valMax){
 else{
 	console.log('Esta fuera del rango establecido')
 }
+
+//Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = falso;
+if(vacaciones || diaDescanso){
+	console.log("El padre puede asistir al juego de su hijo")
+}
+else{
+	console.log("El padre no puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2)
+let numero = 9;
+resultado2 = numero % 2 == 0 ? "Es un numero PAR" : "Es un numero INPAR"
+
+//Convertir String a Number
+let miNumero = "10"; //Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); // Esta es una funcion
+console.log(typeof edad2);
+
+//Funcion isNaN
+if(isNaN(edad2)){ //No es un numero
+	console.log("Esta variable no contiene solo numeros")
+}
+else{
+
+if(edad2 >= 18){
+	console.log("Puede votar");
+}
+else{
+	console.log("Muy joven para votar")
+}
