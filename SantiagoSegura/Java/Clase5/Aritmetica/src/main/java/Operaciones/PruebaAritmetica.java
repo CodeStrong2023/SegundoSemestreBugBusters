f5bbb25f162aa9022bf00b1a6d23ad63fab4d126
Santiago Segura <santiagosegura15@hotmail.com>
2023-11-02 01:29:54 -0300

    Subo las clases 6 de python y java y 7 de js

--- SantiagoSegura/Java/Clase5/Aritmetica/src/main/java/Operaciones/PruebaAritmetica.java
+++ SantiagoSegura/Java/Clase5/Aritmetica/src/main/java/Operaciones/PruebaAritmetica.java
@@ -2,17 +2,38 @@
 package Operaciones;
 
 public class PruebaAritmetica {
-    public static void main(String[] args) {
-        Aritmetica aritmetica1 = new Aritmetica();
-        aritmetica1.a = 3;
-        aritmetica1.b = 7;
-        aritmetica1.sumarNumeros();
+        public static void main(String[] args) {
+
+         int a = 10; //Variables locales
+         int b = 7; // Memoria stack
+         miMetodo(); // Llamamos el nuevo metodo
+         Aritmetica aritmetica1 = new Aritmetica();
+         aritmetica1.a = 3;
+         aritmetica1.b = 7;
+         aritmetica1.sumarNumeros();
+         
+        // Para almacenar un objeto o los atributos se utiliza la memoria 
         
         int resultado = aritmetica1.sumarConRetorno();
         System.out.println("aritmetica1 = " + aritmetica1);
         
         resultado = aritmetica1.sumarConArgumentos(12, 26);
         System.out.println("Resultado usando Argumentos = "+resultado);
+        
+        System.out.println("aritmetica a: "+aritmetica1.a);
+        System.out.println("aritmetica b: "+aritmetica1.b);
+        
+        Aritmetica aritmetica2 = new Aritmetica(5, 8);
+        System.out.println("aritmetica2 = " + aritmetica2.a);
+        System.out.println("aritmetica2 = " + aritmetica2.b);
+        //aritmetica1 = null; nunca utilizar esto, no debe hacer
+        //System.gc(); metodo para limpiar residuos, es pesado, no utilizar
+
+    }
+    
+    public static void miMetodo(){
+       // a = 10; // Una variable esta limitada
+        System.out.println("Aqui hay otro metodo");
     }
     
 }
---
+++ SantiagoSegura/JavaScript/Clase7/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+// Ejercicio para encontrar numeros pares
+let parInpar = 10;
+if(parInpar % 2 == 0){
+	console.log("Es un numero PAR");
+}
+else{
+	console.log("Es un numero INPAR");
+}
+
+// Ejercicio: es mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+	console.log("Usted es una persona adulta");
+}
+else{
+	console.log("Usted es una persona menor de edad")
+}
