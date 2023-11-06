b3d7ccb7847a77fe744940d7c00dca68111abdf8
David Abadie <abadiedaviiid@gmail.com>
2023-11-06 20:02:33 -0300

    poo y herencia

---
+++ DavidAbadie/Java/DavidAbadie/Leccion6/Herencia/src/test/TestHerencia.java
@@ -0,0 +1,22 @@
+
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import domain.Persona;
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Nicolas", 57000.00);
+        System.out.println("empleado1 = " + empleado1);
+        
+//        Date fecha1 = new Date();
+//        
+//        Cliente cliente1 = new Cliente("Nicolas", 'M', 21, "Moyano 123", fecha1, true);
+//        System.out.println("cliente1 = " + cliente1);
+//        
+//        Persona persona1 = new Persona();
+        
+    }
+}
