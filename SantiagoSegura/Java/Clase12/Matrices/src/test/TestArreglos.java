8e8b0db66499c95757b40ea441c903c53161b50b
Santiago Segura <santiagosegura15@hotmail.com>
2023-11-06 21:42:18 -0300

    Realizo la clase 12 de java y la subo al repo

---
+++ SantiagoSegura/Java/Clase12/Matrices/src/test/TestArreglos.java
@@ -0,0 +1,28 @@
+
+package test;
+
+public class TestArreglos {
+    public static void main(String[] args) { //Lado derecho instanciamos un objeto de tipo object
+        int edades [] = new int [3]; //Del lado izquierdo declaramos la variable
+        System.out.println("edades = " + edades);
+        
+        
+        edades[0] = 17;
+        System.out.println("edades 0 = " + edades [0]);
+        
+        edades[1] = 4;
+        System.out.println("edades 0 = " + edades [1]);
+        
+        edades[2] = 28;
+        System.out.println("edades 0 = " + edades [2]);
+        
+        //edades[3] = 7; // Fuera de rango, error en tiempo de ejecucion
+        
+        for(int i = 0; i < edades.length; i++){
+            System.out.println("edades y sus elementos "+i+": "+edades[i]);
+        }
+   
+        
+    }
+    
+}
